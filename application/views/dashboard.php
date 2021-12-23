0fe14ba63511961b64b79213e51328b5bb301730
Wibiati Sekar K <89697985+wsekar@users.noreply.github.com>
2021-12-24 00:32:40 +0700

    Update dashboard.php

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -144,6 +144,41 @@
 							</div>
 						</div>
 					</div>
+						<div class="container">
+						<canvas id="myChart"></canvas>
+					</div>
+					<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+					<script type="text/javascript">
+						var ctx = document.getElementById('myChart').getContext('2d');
+						var chart = new Chart(ctx, {
+							type: 'bar',
+							data: {
+								labels: [
+									<?php
+									if (count($graph) > 0) {
+										foreach ($graph as $data) {
+											echo "'" . $data->nama_barang . "',";
+										}
+									}
+									?>
+								],
+								datasets: [{
+									label: 'Stok Barang',
+									backgroundColor: '#ADD8E6',
+									borderColor: '##93C3D2',
+									data: [
+										<?php
+										if (count($graph) > 0) {
+											foreach ($graph as $data) {
+												echo $data->stok . ", ";
+											}
+										}
+										?>
+									]
+								}]
+							},
+						});
+					</script>
 				</div>
 			</div>
 			<!-- load footer -->
